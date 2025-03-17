288178a96b04c533a699cb46ab3d277d59165bee
fmausberg <felix.mausberg@gmail.com>
2025-03-17 16:52:14 +0100

    enable export

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+module.exports = {
+  exportTrailingSlash: true, // Optional, to add trailing slashes to the URLs
+  // Other configurations if needed
+}
+
 export default nextConfig;
