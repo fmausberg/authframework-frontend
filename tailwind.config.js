03d189508459fb63ff395f2f17e4cab88160e516
fmausberg <felix.mausberg@gmail.com>
2025-03-20 15:59:24 +0100

    tailwind for /app

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
     content: [
-      "./src/**/*.{html,js,jsx,ts,tsx}",
-      "./public/index.html",
+        "./src/**/*.{html,js,jsx,ts,tsx}",
+        "./app/**/*.{js,ts,jsx,tsx}", // Adjust this based on your project structure
+        "./pages/**/*.{js,ts,jsx,tsx}", // Ensure the content paths are correct
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./public/index.html",
     ],
     theme: {
       extend: {},
